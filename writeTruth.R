4e83dea024348e7456ff8c08174e8400e735555c
gdj <gdeanda@inmegen.edu.mx>
2018-04-10 20:34:46 -0500

    modified write truth to have initial node state in first row of output

--- writeTruth.R
+++ writeTruth.R
@@ -3,7 +3,7 @@ for(i in seq_along(tablas)){
   nomen = names(tablas)[i]
   x = tablas[[i]][[2]]
   out_file = paste0(nomen, ".txt")
-  cat(nomen, ncol(x) -1, sep = "\n",file = out_file)
+  cat(0, ncol(x) -1, sep = "\n",file = out_file)
   y = ifelse(x==-3, 1, x)
   write.table(x = y, 
               file = out_file, 
